392c5e70de36c4afcbd8be12b7c719289b69a9e7
Siyad Savad <siyadsavad313@gmail.com>
2023-09-08 10:10:27 +0530

    dashboard page added and product list page added in admin side

--- shoenova/app/urls.py
+++ shoenova/app/urls.py
@@ -5,4 +5,16 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('base/', views.base_view, name='base'),
     path('login-register/', views.login_regis, name='login-register'),
+    path('product-detail/', views.Admn_product_list, name='product-detail'),
+
+
+
+   
+    path('admin-index/', views.adm_index, name='admin-index'),
+    path('admn-product-list/', views.Admn_product_list, name='admn-product-list'),
+
+
+
+    
+
 ]
--- shoenova/app/views.py
+++ shoenova/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 
 # Create your views here.
 
+#user
 def index(request):
     return render(request,'user/index.html')
 
@@ -11,5 +12,13 @@ def base_view(request):
 def login_regis(request):
     return render(request, 'user/page-login-register.html')
 
+def product_details(request):
+    return render(request, 'user/shop-product-left.html')
+
+
+#admin
 def adm_index(request):
-    return render(request, '')
+    return render(request, 'admin/index.html')
+
+def Admn_product_list(request):
+    return render(request, 'page-products-list.html')
