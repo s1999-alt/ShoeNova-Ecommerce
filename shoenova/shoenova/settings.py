1e38b6f3f6a205d980bbaf489ca6cacd54341c43
Siyad Savad <siyadsavad313@gmail.com>
2023-10-24 20:41:25 +0530

    Static root is added

--- shoenova/shoenova/settings.py
+++ shoenova/shoenova/settings.py
@@ -139,6 +139,7 @@ import os
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectstatic')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
