5a48bc3ae99e96392209d039b54b69dd0ca8389c
Siyad Savad <siyadsavad313@gmail.com>
2023-11-20 19:41:42 +0530

    changed debug in settings to TRUE

--- shoenova/shoenova/settings.py
+++ shoenova/shoenova/settings.py
@@ -27,7 +27,7 @@ AUTH_USER_MODEL = 'app.UserProfile'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
