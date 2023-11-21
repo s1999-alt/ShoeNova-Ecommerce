a9a3f40b079b4eddf19d525f9bb70d2ec96fbc42
Siyad Savad <siyadsavad313@gmail.com>
2023-11-21 16:47:48 +0530

    404 page shows when logout solve in locally

--- shoenova/shoenova/urls.py
+++ shoenova/shoenova/urls.py
@@ -32,5 +32,5 @@ urlpatterns = [
 handler404 = 'app.views.error_404'
 
 
-# if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
